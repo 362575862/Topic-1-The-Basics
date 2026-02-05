59c50f7f0ef48cd254a08a636f45a9a160592239
362575862 <362575862@2260I-L29-027>
2026-02-05 12:17:47 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Topic_1_The_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+
+        }
+    }
+}
