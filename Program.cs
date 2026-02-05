a5471936bb6f61a37c0c6bda8a160a3923cac33f
Aiden Patterson <362575862@GoTVDSB.ca>
2026-02-05 12:57:41 -0500

    Done

--- Program.cs
+++ Program.cs
@@ -5,11 +5,38 @@
         static void Main(string[] args)
         {
             // Aiden
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Hello, World!");
 
+            /*the code will say my name
+             * watch out */
+            Console.WriteLine("My name is Aiden");
 
+            Console.Write("Computers");
+            Console.Write(" ");
+            Console.WriteLine("Rule");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+            Console.ResetColor();
+            Console.WriteLine("My favourite food is pizza");
 
-
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(" ");
+            Console.WriteLine("  ");
+            Console.WriteLine("   ");
+            Console.ResetColor();
+            Console.ForegroundColor= ConsoleColor.Blue;
+            Console.WriteLine("its thursday today");
+            Console.ResetColor();
         }
+
+
+
+
+
     }
+
+
 }
